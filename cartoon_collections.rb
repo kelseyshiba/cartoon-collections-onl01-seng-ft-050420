d2bbc128ab0d0bec1fd0025ad501f8974ee891f6
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 19:16:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any?
+  array.any? do |word|
+    word.length > 4
 end
 
 def find_the_cheese# code an argument here
