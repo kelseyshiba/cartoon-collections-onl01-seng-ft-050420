aa206017a0e90e887e73f248ea98467405691ce6
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 19:36:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,9 +21,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?(
-    array.each do |cheese|
-      cheese)
+  array.include?(cheese_types)
     end
   end
 end
