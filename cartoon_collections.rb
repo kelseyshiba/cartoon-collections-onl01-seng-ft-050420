abc8fe4ccdbf68dc5af9eb89a5fa5679fdbd990c
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 19:32:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.include?("cheddar", "gouda", "camembert")
   binding.pry
 end
