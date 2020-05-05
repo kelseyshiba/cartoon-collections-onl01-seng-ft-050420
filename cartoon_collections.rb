7a26eca63c58978969dcd3f8b8a1581a7b61e641
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 19:52:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |cheese|
-    array.include?(cheese_types) ? true: nil
-  end
+  cheese_types.include?(array)
 end
