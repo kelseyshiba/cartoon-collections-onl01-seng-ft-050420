d454060e9942931a894b6a3e946a05b5aa1ea329
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 19:07:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  summon = []
+  array.map do |element|
+    summon << "#{element.capitalize}!"
+  end 
+  summon
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include?(true) do |words|
+      if word.length > 4
+        true
+      else 
+      false
+    end
+  end 
 end
 
 def find_the_cheese# code an argument here
